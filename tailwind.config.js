bc242ff5c21dabe3d765dabd804ececdfee9e5b2
Alberto Paim <119620474+AlbertoPaim@users.noreply.github.com>
2023-06-10 13:44:52 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,ts,js,css}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'azul': '#0000ff'
+      }
+    },
+  },
+  plugins: [],
+}
+
